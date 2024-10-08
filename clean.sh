cd83bcb46e24d31e086ab781b2b500dc29d6a20f
RabbitQ <ra66itq@gmail.com>
2024-10-08 18:23:27 +0800

    修改交互问题并添加清理前后对比

--- clean.sh
+++ clean.sh
@@ -13,13 +13,28 @@ echo "
 echo "--------------------------------------------------------------------------------"
 echo -e "\n"
 
-
-  
 # 定义日志文件
 LOGFILE="cleanup.log"
 # 清空或创建日志文件
 > "$LOGFILE"
+
+used_space_before=$(df -k / | awk 'NR==2 {used=$3; if (used >= 1048576) {printf("%.2f GB\n", used/1048576)} else if (used >= 1024) {printf("%.2f MB\n", used/1024)} else {printf("%d KB\n", used)}}')
+
 {
+ 
+echo "======== 清理旧内核文件 ========"
+
+# 获取当前正在使用的内核版本（不包括版本后缀）
+CURRENT_KERNEL=$(uname -r | sed 's/-[^-]*$//')
+# 列出所有linux-*包，排除正在使用的内核，排除那些不包含数字的包名
+PACKAGES=$(dpkg -l 'linux-*' | awk '/^ii/ && !/'"$CURRENT_KERNEL"'/ {print $2}')
+# 如果PACKAGES不为空，则使用apt-get purge删除它们
+if [ -n "$PACKAGES" ]; then
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y purge $PACKAGES
+fi
+echo "======== 清理旧内核文件完成 ========"
+echo -e "\n\n"
+ 
 echo "======== 清理临时文件 ========"
 rm -rf /tmp/*
 echo "======== 清理临时文件完成 ========"
@@ -31,7 +46,6 @@ apt autoclean
 apt autoremove
 echo "======== 清理软件包缓存及不需要的软件包完成 ========"
 echo -e "\n\n"
-
 
 echo "======== 删除孤立的软件包 ========"
 sudo apt-get autoremove --purge
@@ -68,11 +82,6 @@ for log in $logs
 echo "======== 清理容器日志完成 ========"
 echo -e "\n\n"
 
-echo "======== 清理旧内核文件 ========"
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'.*/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
-echo "======== 清理旧内核文件完成 ========"
-echo -e "\n\n"
-
 echo "======== 清理缓存和日志 ========"
 find /var/cache -type f -exec rm -rf {} \;
 find /var/log -type f | while read f; do echo -n '' > $f; done
@@ -95,7 +104,7 @@ echo "======== 清理孤立的库文件完成 ========"
 echo -e "\n\n"
 
 echo "======== 清理过时的内核头文件和头文档 ========"
-sudo apt-get autoremove --purge
+apt-get autoremove --purge
 echo "======== 清理过时的内核头文件和头文档完成 ========"
 echo -e "\n\n"
 
@@ -123,4 +132,10 @@ echo "======== 查找大文件（请自动删除） ========"
 du -h / | grep '[0-9]G'
 echo "======== 查找大文件完成 ========"
 echo -e "\n\n"
+
+# 获取清理后的大小
+used_space_after=$(df -k / | awk 'NR==2 {used=$3; if (used >= 1048576) {printf("%.2f GB\n", used/1048576)} else if (used >= 1024) {printf("%.2f MB\n", used/1024)} else {printf("%d KB\n", used)}}')
+echo "Used space before cleanup: $used_space_before"
+echo "Used space after cleanup: $used_space_after"
+
 }  2>&1 | tee -a "$LOGFILE"
